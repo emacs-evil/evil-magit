e7964937c4b1f4992de2847230d1ccf4437ee89b
Jonas Bernoulli <jonas@bernoul.li>
2015-09-01 00:48:13 +0200

    import the stub

---
+++ README.md
@@ -0,0 +1,21 @@
+Black magic
+===========
+
+This library configures Magit and Evil to play well with each
+other.  Or rather that's what it is supposed to do eventually.
+
+Users are encouraged to create a branch and show each other how
+they have configured these two packages to play well with each
+other.
+
+I hope that eventually all the black magitians can collectively
+come up with a configuration that works for most users, or that
+we end up with a collection of reasonable configurations.
+
+For a general discussion see https://github.com/magit/evil-magit/issues/1.
+
+Branches
+========
+
+* `master` This is just a stub.  Eventually one of the other branches
+  hopefully become the real `master`.
---
+++ evil-magit.el
@@ -0,0 +1,47 @@
+;;; evil-magit.el --- doing black magic
+
+;; Copyright (C) 2015  The Magit Project Contributors
+
+;; Package-Requires: ((evil "1.2.3") (magit "2.2.2"))
+;; Homepage: https://github.com/magit/evil-magit
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library configures Magit and Evil to play well with each
+;; other.  Or rather that's what it is supposed to do eventually.
+
+;; Users are encouraged to create a branch and show each other how
+;; they have configured these two packages to play well with each
+;; other.
+
+;; I hope that eventually all the black magitians can collectively
+;; come up with a configuration that works for most users, or that
+;; we end up with a collection of reasonable configurations.
+
+;; For a general discussion see
+;; https://github.com/magit/evil-magit/issues/1.
+
+;;; Code:
+
+(require 'evil)
+(require 'magit)
+
+;;; evil-magit.el ends soon
+(provide 'evil-magit)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+;;; evil-magit.el ends here
