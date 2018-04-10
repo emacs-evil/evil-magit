8c7dc89c2b60f927bd558192648113aa1f82da35
Michael Stergianis <michaelstergianis@gmail.com>
2018-04-10 02:04:18 -0400

    Reintroduce magit-blame-mode as it still exists.

--- evil-magit.el
+++ evil-magit.el
@@ -130,6 +130,7 @@ should be a string suitable for `kbd'."
 
 (defvar evil-magit-untouched-modes
   '(git-popup-mode
+    magit-blame-mode
     magit-blame-read-only-mode
     magit-popup-mode
     magit-popup-sequence-mode)
