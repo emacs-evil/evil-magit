c3eae3b2347c4b1798d6d05de18ad073dbbb27d3
Justin Burkett <justin@burkett.cc>
2019-01-17 06:58:17 -0500

    Move pulling key in magit-dispatch-popup with forge loaded
    Put "p"-pull before "P"-push

--- evil-magit.el
+++ evil-magit.el
@@ -638,7 +638,8 @@ using `evil-magit-toggle-text-mode'"
      (evil-magit-define-key evil-magit-state 'magit-mode-map "P" 'magit-push-popup)
      (evil-magit-define-key evil-magit-state 'magit-mode-map "F" 'forge-dispatch)
      (magit-change-popup-key 'magit-dispatch-popup :actions ?p ?P)
-     (magit-change-popup-key 'magit-dispatch-popup :actions ?F ?p)
+     (magit-remove-popup-key 'magit-dispatch-popup :actions ?F)
+     (magit-define-popup-action 'magit-dispatch-popup ?p "Pulling" 'magit-pull-popup ?P t)
      (magit-define-popup-action 'magit-dispatch-popup ?F "Forge" 'forge-dispatch ?f)))
 
 ;;; evil-magit.el ends soon
